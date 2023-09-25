b62a9529525b204ed68de06b82a39ae13f61222f
Huang Yu Hao <yuhao.huang@ntu.edu.sg>
2023-09-25 16:28:27 +0800

    added images and onHover

--- src/Components/DeckBuilder/DeckBuilder.js
+++ src/Components/DeckBuilder/DeckBuilder.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { sample_cards } from "../../TestData/sampleData";
+import "./cards.css";
 
 export default function DeckBuild() {
+  const [hoveredImage, setHoveredImage] = useState(null);
+  const [hoveredEffect, setHoveredEffect] = useState(null);
+
+  const onHover = (card) => {
+    setHoveredImage(card.Image);
+    setHoveredEffect(card.Effect);
+  };
+  const onHoverOut = () => {
+    setHoveredImage(null);
+    setHoveredEffect(null);
+  };
   return (
     <div>
       <h1>This the page for deck building</h1>
+      {hoveredImage && (
+        <div className="preview-image">
+          <img src={hoveredImage} alt="Selected Card" />
+          <p>{hoveredEffect}</p>
+        </div>
+      )}
+      {Object.values(sample_cards).map((card, index) => (
+        <button
+          className="card-img"
+          key={index}
+          onMouseOver={() => onHover(card)}
+          onMouseOut={() => onHoverOut()}
+        >
+          <img src={card.Image} alt="card" />
+        </button>
+      ))}
+
       <br />
       <button>Save deck</button>
       <button>
