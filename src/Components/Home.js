02288083677d57b8cd8d6a018c0b129ee302e1c4
unknown <yuhao.huang@ntu.edu.sg>
2023-10-10 13:09:16 +0800

    minor update

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { listOfDecks } from "../TestData/sampleData";
+//import { listOfDecks } from "../TestData/sampleData";
+
+const URL = process.env.REACT_APP_BACKEND_URL;
+const url_decks = `${URL}/deck`;
 
 export default function HomePage() {
+  const [deckList, setDeckList] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url_decks, {
+          method: "GET",
+        });
+
+        const data = await response.json();
+        setDeckList(data);
+      } catch (error) {
+        console.error("Error: ", error.message);
+      }
+    };
+    if (deckList === null) {
+      fetchData();
+    }
+  });
+
   return (
     <div>
       <h1>This the Home Page</h1>
       <h3>List of your decks:</h3>
-      <ul>
-      {Object.values(listOfDecks).map((deck)=>(<li><button><Link to={`/analysis?deck=${deck.name}`}>{deck.name}</Link></button></li>))}
-      </ul>
+      {console.log(deckList)}
+      {deckList && (
+        <ul>
+          {Object.values(deckList).map((deck) => (
+            <li>
+              <button>
+                <Link to={`/analysis?deck=${deck.deck_name}`}>
+                  {deck.deck_name}
+                </Link>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+
       <br />
       <button>
         <Link to="/deckbuilder"> Build new deck</Link>
