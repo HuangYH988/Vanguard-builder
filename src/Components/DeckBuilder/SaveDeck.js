4b1019d90f71b63f1a7e3705babab0929d554db7
unknown <yuhao.huang@ntu.edu.sg>
2023-10-23 00:07:53 +0800

    added getToken to saveDeck

--- src/Components/DeckBuilder/SaveDeck.js
+++ src/Components/DeckBuilder/SaveDeck.js
@@ -4,37 +4,49 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const URL = process.env.REACT_APP_BACKEND_URL;
 const url = `${URL}/deck`;
-const url_player=`${URL}/player`;
+const url_player = `${URL}/player`;
 
 export default function SaveDeck(prop) {
   const { isOpen, onClose, rideDeck, triggers, mainDeck } = prop;
   const [deckName, setDeckName] = useState("");
-const {user, isAuthenticated } = useAuth0();
-const [player, setPlayer] =useState(null);
+  const { user, isAuthenticated, getAccessTokenSilently, loginWithRedirect } =
+    useAuth0();
+  const [ accessToken, setAccessToken ] = useState("");
+  const [player, setPlayer] = useState(null);
 
-useEffect(()=>{
-  const fetchPlayerData=async()=>{
-    try {
-      const response = await fetch(url_player, {
-        method: "GET",
-      });
-
-      const data = await response.json();
-      for (const acc in data) {
-        if (user.nickname === data[acc].player_name) {
-          setPlayer(data[acc])
-          break;
-        }
-      }
-    } catch (error) {
-      console.error("Error: ", error.message);
-    }
+const checkUser=async()=>{
+  if (isAuthenticated) {
+    let token = await getAccessTokenSilently();
    
+    setAccessToken(token)
+  } else {
+    loginWithRedirect();
   }
-  if(isAuthenticated && !player){
-    fetchPlayerData();
-  }
-})
+}
+
+  useEffect(() => {
+    checkUser();
+    const fetchPlayerData = async () => {
+      try {
+        const response = await fetch(url_player, {
+          method: "GET",
+        });
+
+        const data = await response.json();
+        for (const acc in data) {
+          if (user.nickname === data[acc].player_name) {
+            setPlayer(data[acc]);
+            break;
+          }
+        }
+      } catch (error) {
+        console.error("Error: ", error.message);
+      }
+    };
+    if (isAuthenticated && !player) {
+      fetchPlayerData();
+    }
+  });
 
   const deckList2 = [];
   const rideDeckList = [rideDeck.g0, rideDeck.g1, rideDeck.g2, rideDeck.g3];
@@ -68,10 +80,10 @@ useEffect(()=>{
   };
   async function saveDeck(e) {
     e.preventDefault();
-    if(!isAuthenticated){
-      alert("You need to login first before you can save a deck list.")
+    if (!isAuthenticated) {
+      alert("You need to login first before you can save a deck list.");
     }
-  
+
     // Trim the deckName and check if it's empty or contains only spaces
     const trimmedDeckName = deckName.trim();
     if (!trimmedDeckName) {
@@ -90,18 +102,18 @@ useEffect(()=>{
         ride_deck: rideDeckList,
         triggers: triggersList,
       };
-      
+
       try {
         const response = await fetch(url, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
             // Add Authorization header if needed
-            // "Authorization": `Bearer ${accessToken}`,
+             "Authorization": `Bearer ${accessToken}`,
           },
           body: JSON.stringify(requestData),
         });
-  
+
         if (!response.ok) {
           const errorData = await response.json();
           console.error("Error:", errorData);
@@ -116,7 +128,6 @@ useEffect(()=>{
       }
     }
   }
-  
 
   const handleCloseModal = () => {
     onClose();
